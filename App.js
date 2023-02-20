f763702ca2d2d0f7c55c81606130272bb1782709
Martin Iglesias <martin.iglesias19@gmail.com>
2023-02-20 18:43:37 -0300

    First modification to App.js
    Signed-off-by: Martin Iglesias <martin.iglesias19@gmail.com>

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First App on React Native!!!</Text>
+      <Text>Primer App en React Native!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
